0baa2d8610b4cb82173e4384b83f2a0f65deb284
Kalum Jacob Ost <kalumost@gmail.com>
2022-04-20 00:52:34 -0600

    Improve documentation for ItemAttributeLookup

--- library/attribute/lookup/src/main/java/org/quiltmc/qsl/access/api/item/ItemAttributeLookup.java
+++ library/attribute/lookup/src/main/java/org/quiltmc/qsl/access/api/item/ItemAttributeLookup.java
@@ -31,25 +31,82 @@ import org.quiltmc.qsl.access.impl.item.ItemAttributeLookupImpl;
  * @param <C> The type of context object required to execute the lookup.
  */
 public interface ItemAttributeLookup<A, C> {
+	/**
+	 * Gets a {@link ItemAttributeLookup} instance associated with the specified identifier
+	 * @param id The unique identifier of the lookup
+	 * @param attributeClass The attribute class the lookup should query for
+	 * @param contextClass The context required for the lookup to query successfully
+	 * @return The unique lookup associated with the specified identifier
+	 * @throws IllegalAccessException If a lookup with matching the identifier
+	 *  exists, but for an incorrect {@code attributeClass} or {@code contextClass}
+	 */
 	static <A, C> ItemAttributeLookup<A, C> get(Identifier id, Class<A> attributeClass, Class<C> contextClass) throws IllegalAccessException {
 		return ItemAttributeLookupImpl.get(id, attributeClass, contextClass);
 	}
 
+	/**
+	 * Attempt to query the ItemStack for the attribute handled by this lookup
+	 *
+	 * <p>Note that the attribute that is returned may not allow the original
+	 * ItemStack to be modified. Attribute authors are encouraged to create
+	 * documentation to establish whether this is the case or not.
+	 * <br>Likewise, providers may capture a reference to the original stack,
+	 * but should not modify it directly
+	 *
+	 * @param stack The {@code ItemStack} to query for the attribute
+	 * @param context The context of the query, as defined by the type parameter {@code C}
+	 * @return The retrieved Attribute instance, or {@code null} if no such
+	 *  instance could be queried
+	 */
 	@Nullable
 	A find(ItemStack stack, C context);
 
+	/**
+	 * Mark the specified items as being capable of holding the Attribute
+	 * associated with this Lookup, for querying via this Lookup
+	 *
+	 * @param items The item types to associate with the Attribute type
+	 * @throws IllegalArgumentException IF the Attribute is not assignable from
+	 *  the class of one of the items provided
+	 */
 	void registerSelf(ItemConvertible... items);
 
+	/**
+	 * Mark specified items as being capable of holding a specific attribute,
+	 * to be handled using the specified {@code ItemAttributeProvider}
+	 *
+	 * @param provider The provider to register with
+	 * @param items The items to register
+	 */
 	void registerForItems(ItemAttributeProvider<A, C> provider, ItemConvertible... items);
 
+	/**
+	 * Register a provider to be used when a standard provider cannot be found
+	 * with a provided item. Providers registered this way are checked for all
+	 * queries which fail to a provider for the requested item via the usual way
+	 * @param provider The provider
+	 */
 	void registerFallback(ItemAttributeProvider<A, C> provider);
 
+	/**
+	 * Get the identifier for this lookup
+	 */
 	Identifier getId();
 
+	/**
+	 * Get the attribute class for this lookup
+	 */
 	Class<A> attributeClass();
 
+	/**
+	 * Get the context class for this lookup
+	 */
 	Class<C> contextClass();
 
+	/**
+	 * Return the registered provider associated with the provided item, if one exists
+	 * Queries should go through {@link #find}, only use this for the purposes of inspection!
+	 */
 	@Nullable
 	ItemAttributeProvider<A, C> getProvider(Item item);
 
